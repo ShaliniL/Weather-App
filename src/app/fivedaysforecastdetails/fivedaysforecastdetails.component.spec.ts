43a9b76d915db5dd853f1996d9d28cc88f8d04ec
shalini <shalini4mysore@gmail.com>
2020-06-07 19:16:37 +0530

    First commit

--- src/app/fivedaysforecastdetails/fivedaysforecastdetails.component.spec.ts
+++ src/app/fivedaysforecastdetails/fivedaysforecastdetails.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainDeatilsComponent } from './main-deatils.component';
+import { DetailsComponent } from './fivedaysforecastdetails.component';
 
-describe('MainDeatilsComponent', () => {
-  let component: MainDeatilsComponent;
-  let fixture: ComponentFixture<MainDeatilsComponent>;
+describe('DetailsComponent', () => {
+  let component: DetailsComponent;
+  let fixture: ComponentFixture<DetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainDeatilsComponent ]
+      declarations: [ DetailsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainDeatilsComponent);
+    fixture = TestBed.createComponent(DetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
